6b6d9537e1e364344e941ee906b3980fda621327
Noah McConnell <nonomcconnell2@gmail.com>
2018-09-11 13:30:05 -0600

    logins

--- client/src/views/Customer-Login.vue
+++ client/src/views/Customer-Login.vue
@@ -1,24 +1,50 @@
 <template>
   <div class="customer-login">
-    <form class="">
-      <input type="email" v-model="" placeholder="email">
-      <input type="password" v-model="" placeholder="password">
+    <form class="" v-if="customerLoginForm" @submit.prevent="loginCustomer">
+      <input type="email" v-model="creds.email" placeholder="email">
+      <input type="password" v-model="creds.password" placeholder="password">
       <button type="submit">Login</button>
     </form>
-    <form class="">
-      <input type="name" v-model="" placeholder="name">
-      <input type="tel" v-model="" placeholder="phone">
-      <input type="email" v-model="" placeholder="email">
-      <input type="password" v-model="" placeholder="password">
+    <form class="" v-else @submit.prevent="registerCustomer">
+      <input type="name" v-model="newCustomer.name" placeholder="name">
+      <input type="tel" v-model="newCustomer.phone" placeholder="phone">
+      <input type="email" v-model="newCustomer.email" placeholder="email">
+      <input type="password" v-model="newCustomer.password" placeholder="password">
       <button type="submit">Create Account</button>
-    <h3>Already a member? Login.</h3>
+    <div @click="customerLoginForm = !customerLoginForm">
+      <p v-if="customerLoginForm">Not a member? Click here to register</p>
+      <p v-else>Already a member? Click here to Login</p>
+    </div>
   </div>  
 </template>
 
 <script>
 export default {
-  
-}
+  name: "customerLogin",
+  data() {
+    return{
+      customerLoginForm: true,
+      creds: {
+        email: "",
+        password: "",
+      },
+      newCustomer: {
+        name: "",
+        phone: "",
+        email: "",
+        password: "",
+      }
+    }
+  },
+  methods: {
+    registerCustomer(){
+      this.$store.dispatch()
+    },
+    loginCustomer(){
+      this.$store.dispatch()
+    }
+  }
+};
 </script>
 
 <style>
---
+++ client/src/views/Provider-Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="provider-login">
+    <form class="" v-if="providerLoginForm" @submit.prevent="loginProvider">
+      <input type="email" v-model="creds.email" placeholder="email">
+      <input type="password" v-model="creds.password" placeholder="password">
+      <button type="submit">Login</button>
+    </form>
+    <form class="" v-else @submit.prevent="registerProvider">
+      <input type="name" v-model="newProvider.name" placeholder="name">
+      <input type="tel" v-model="newProvider.phone" placeholder="phone">
+      <input type="email" v-model="newProvider.email" placeholder="email">
+      <input type="password" v-model="newProvider.password" placeholder="password">
+      <button type="submit">Create Account</button>
+    <div @click="providerLoginForm = !providerLoginForm">
+      <p v-if="providerLoginForm">Not a  registered provider? Click here to register</p>
+      <p v-else>Already registered? Click here to Login</p>
+    </div>
+  </div>  
+</template>
+
+
+<script>
+export default {
+    name: "providerLogin",
+  data() {
+    return{
+      providerLoginForm: true,
+      creds: {
+        email: "",
+        password: "",
+      },
+      newProvider: {
+        name: "",
+        phone: "",
+        email: "",
+        password: "",
+      }
+    }
+  },
+  methods: {
+    registerProvider(){
+      this.$store.dispatch()
+    },
+    loginProvider(){
+      this.$store.dispatch()
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
