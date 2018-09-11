cc50831a9c72f3c75409ab55edd7eb12311da575
matt <matthew.dobrowolski5@gmail.com>
2018-09-11 15:21:53 -0600

    providerRouter

---
+++ server/routes/provider.js
@@ -0,0 +1,45 @@
+let router = require('express').Router()
+let Provider = require('../models/provider')
+
+router.get('/',(req,res,next)=>{
+    Provider.find({providerId: req.session.uid})
+    .then (data=>{
+        res.send(data)
+    })
+    .catch(err=>{
+    console.log(err)
+    next()
+    })
+    })
+    router.post('/',(req, res, next)=>{
+        req.body.providerId = req.sesssion.uid
+        Provider.create(req.body)
+        .then(newReq=>{
+            res.send(newReq)
+        })
+        .catch(err=>{
+            res.status(400).send(err)
+            next()
+        })
+    })
+    router.put('/:id',(req,res,next)=>{
+        Provider.findById(req.params.id)
+        .then(provider=>{
+            provider.update(req.body,(err)=>{
+                if(err){
+                res.status(400).send(err)
+                next()
+                return
+                }
+                res.send("Success")
+            })
+        })
+    })
+    router.delete('/:id',(req , res , next)=>{
+        Provider.findByIdAndRemove(req.params.id)
+        .then(data=>{
+            res.send('deleted')
+        })
+    })
+    
+    module.exports = router
