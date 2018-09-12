60977438b31bbb2898c293ed24ab13338e56be2f
k-cormack <k.cormack@gmail.com>
2018-09-12 16:27:18 -0600

    UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="flex" id="app">
+  
     <router-view/>
+
   </div>
 </template>
+
+<script>
+
+</script>
+
+<style>
+
+  body {
+    background: url('./assets/plowDriveway.jpg');
+    background-size: cover;
+    background-attachment: fixed
+  }
+  #app {
+    color: white;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    text-shadow: 2px 2px black;
+    
+  }
+
+</style>
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,30 +1,51 @@
 <template>
   <div class="home">
-    <h1>Looking for help or trying to help?</h1>
-    <button @click="customerLogin" type="button" class="btn btn-primary btn-lg">Customer</button>
-    <button @click="providerLogin" type="button" class="btn btn-success btn-lg">Provider</button>
+    <div class="col-12 main">
+    <div class="row">
+
+      <h1>Looking for Service?</h1>
+    </div>
+    <div class="row">
+      <button @click="customerLogin" type="button" class="btn btn-primary btn-lg">Customer</button>
+    </div>
+  </div>
+
+    <div class="row">
+      <h1>Looking for Plowing Job?</h1>
+    </div>
+    <div class="row">
+      <button @click="providerLogin" type="button" class="btn btn-success btn-lg">Provider</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name:"Home",
-  methods:{
-    customerLogin(){
-      this.$router.push({name: "customerLogin"})
-    },
-    providerLogin(){
-      this.$router.push({name: "providerLogin"})
+  export default {
+    name: "Home",
+    methods: {
+      customerLogin() {
+        this.$router.push({ name: "customerLogin" })
+      },
+      providerLogin() {
+        this.$router.push({ name: "providerLogin" })
+      }
     }
   }
-}
 </script>
 
-<style>
-.btn-lg{
-  width: 25%;
-  height: 13vh;
-  font-size: 44px
-}
+<style scoped>
+  .btn-lg {
+    width: 250px;
+    height: 120px;
+    font-size: 44px;
+    text-shadow: 2px 2px black;
+    padding-bottom: 15px
+  }
 
+  .row {
+    justify-content: center
+  }
+  .col-12 {
+    margin-top: 25vh
+  }
 </style>
