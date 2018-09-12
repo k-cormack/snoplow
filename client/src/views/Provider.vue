9cbcdb42dd0a7fd9505c8a17789baa8198f5e2e2
Noah McConnell <nonomcconnell2@gmail.com>
2018-09-12 16:29:46 -0600

    little stuff

--- client/src/views/Provider.vue
+++ client/src/views/Provider.vue
@@ -14,6 +14,7 @@
 
     import googleMaps from "@/components/Google-Map.vue"
     import Navbar from '@/components/Provider-Navbar'
+    
     export default {
         name: 'Provider',
         components: {
