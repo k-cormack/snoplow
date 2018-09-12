b09d2c4083557e70d73e25d5163e4f6dc7b8f1c2
k-cormack <k.cormack@gmail.com>
2018-09-12 13:27:34 -0600

    cust-login mod

---
+++ client/src/views/Customer-Info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="customer">
+      <h1>Pending Jobs</h1>
+      <div>
+  
+      </div>
+      <h1>Active Jobs</h1>
+      <div>
+  
+      </div>
+      <h1>Completed Jobs</h1>
+      <div>
+  
+      </div>
+    </div>
+  </template>
+  
