e2cc64ad41fb7d3751d4e29a245b7a99c9950fa2
k-cormack <k.cormack@gmail.com>
2018-09-13 14:09:35 -0600

    cust/prov info css

--- client/src/views/Customer-Info.vue
+++ client/src/views/Customer-Info.vue
@@ -3,35 +3,39 @@
     <div>
       <Navbar />
     </div>
-    <h1>Pending Jobs</h1>
-  <div>
-  
-  </div>
-    <h1>Active Jobs</h1>
-  <div>
-  
-  </div>
-    <h1>Completed Jobs</h1>
-  <div>
-  
+    <div class="container">
+      <div>
+        <h1>Pending Jobs</h1>
+
+      </div>
+      <div>
+        <h1>Active Jobs</h1>
+
+      </div>
+      <div>
+        <h1>Completed Jobs</h1>
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 
-import Navbar from '@/components/Customer-Navbar';
+  import Navbar from '@/components/Customer-Navbar';
 
-export default {
+  export default {
     name: "customerInfo",
     components: {
-        Navbar,
+      Navbar,
     },
-}    
-  
+  }
+
 </script>
 
 <style scoped>
+  .container {
+    background-color: rgba(0, 68, 255, 0.555);
+  }
 
 </style>
-  
