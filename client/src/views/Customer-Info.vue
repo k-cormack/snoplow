a9b45e36be16d64408faf4e387a9edb4213b5bf5
Chace Hartwell <chace.l.hartwell@icloud.com>
2018-09-13 10:53:44 -0600

    Added Nav bar to info pages

--- client/src/views/Customer-Info.vue
+++ client/src/views/Customer-Info.vue
@@ -1,21 +1,33 @@
 <template>
-    <div class="customer">
-      <h1>Pending Jobs</h1>
-      <div>
-  
-      </div>
-      <h1>Active Jobs</h1>
-      <div>
-  
-      </div>
-      <h1>Completed Jobs</h1>
-      <div>
-  
-      </div>
+  <div class="customer">
+    <div>
+      <Navbar />
     </div>
-  </template>
+    <h1>Pending Jobs</h1>
+  <div>
+  
+  </div>
+    <h1>Active Jobs</h1>
+  <div>
+  
+  </div>
+    <h1>Completed Jobs</h1>
+  <div>
+  
+    </div>
+  </div>
+</template>
 
-  <script>
+<script>
+
+import Navbar from '@/components/Customer-Navbar';
+
+export default {
+    name: "customerInfo",
+    components: {
+        Navbar,
+    },
+}    
   
 </script>
 
