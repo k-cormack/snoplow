533796726adb295c335b7e4755ed90920ff04f14
matt <matthew.dobrowolski5@gmail.com>
2018-09-14 10:54:13 -0600

    cool

--- server/server-assets/auth/routes.js
+++ server/server-assets/auth/routes.js
@@ -50,6 +50,7 @@ router.post('/auth/login', (req, res) => {
     email: req.body.email
   })
     .then(user => {
+      console.log(user)
       if (!user) {
         return res.status(400).send(loginError)
       }
