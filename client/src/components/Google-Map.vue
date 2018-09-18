b3a2b26fc7b1c4b79782948b873a7a901cf3f1ba
k-cormack <k.cormack@gmail.com>
2018-09-17 20:33:35 -0600

    maps infoWindow

--- client/src/components/Google-Map.vue
+++ client/src/components/Google-Map.vue
@@ -1,65 +1,103 @@
 <template>
-    <div>
+  <div>
     <div id="googleMap">
     </div>
-        {{myMapData}}
+    {{myMapData}}
 
-    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "googleMap",
-  props: ["mapData"],
-  data: function() {
-    return {
-      mapName: this.name + "-map",
-      markerCoordinates: [
-        {
-          latitude: 43.615,
-          longitude: -116.2023
-        },
-        {
-          latitude: 43.62,
-          longitude: -116.2023
-        }
-      ]
-    };
-  },
-  computed: {
-    myMapData() {
-      this.$store.state.map;
-    }
-  },
-  mounted: function() {
-    const element = document.getElementById("googleMap");
-    var options = {
-      zoom: 14,
-      center: new google.maps.LatLng(43.615, -116.2023),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+  export default {
+    name: "googleMap",
+    props: ["mapData"],
+    data: function () {
+      return {
+        mapName: this.name + "-map",
+        markers: [],
+        markerCoordinates: [
+          {
+            latitude: 43.615,
+            longitude: -116.2023
+          },
+          {
+            latitude: 43.62,
+            longitude: -116.2023
+          }
+        ]
+      };
+    },
+    
+    computed: {
+      myMapData() {
+        this.$store.state.map;
+      }
+    },
+    mounted: function () {
+      const element = document.getElementById("googleMap");
+      var options = {
+        zoom: 14,
+        center: new google.maps.LatLng(43.615, -116.2023),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
 
-    var map = new google.maps.Map(element, options);
+      var map = new google.maps.Map(element, options);
 
-    this.markerCoordinates.forEach((coord, index) => {
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(coord.latitude, coord.longitude),
-        title: "Stupid loop "+ index,
-        visible: true
+      this.markerCoordinates.forEach((coord, index) => {
+        
+        var content = 
+          '</div>' +
+          '<button @click="postJob()">POST</button>' +
+          '</div>';
+
+        var locationInfowindow = new google.maps.InfoWindow({
+          content: content,
+          maxWidth: 300,
+        });
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(coord.latitude, coord.longitude),
+          title: "Stupid loop " + index,
+          visible: true,
+          infowindow: locationInfowindow
+        });
+
+        marker.setMap(map);
+
+        marker.addListener('click', (event) => {
+          this.closeAll()
+          marker.infowindow.open(map, marker);
+        });
+
+        google.maps.event.addListener(map, 'click', function () {
+          marker.infowindow.close();
+        });
+
+        this.markers.push(marker)
       });
-
-      marker.setMap(map);
-      
-    });
-  }
-};
+    },
+    methods: {
+      postJob() {
+        debugger
+        this.$store.dispatch('postJob', marker.position)
+      },
+      closeAll(){
+        this.markers.forEach(m=>{
+          m.infowindow.close();
+        })
+      }
+    }
+  };
 </script>
 
-<style scoped>
-#googleMap {
-  display: flex;
-  width: 70vw;
-  height: 90vh;
-  background: gray;
-}
+<style>
+  #googleMap {
+    display: flex;
+    width: 70vw;
+    height: 90vh;
+    background: gray;
+  }
+
+  #iw-container {
+    color: black
+  }
 </style>
