d8bc333de505141e0e68f91576292d818ce9f9ff
k-cormack <k.cormack@gmail.com>
2018-09-18 15:59:47 -0600

    romoved unecessary function

--- client/src/components/Google-Map.vue
+++ client/src/components/Google-Map.vue
@@ -13,7 +13,7 @@
     data: function () {
       return {
         mapName: this.name + "-map",
-        map:{}
+        map: {}
       };
     },
 
@@ -35,80 +35,73 @@
 
       this.drawMarkers()
     },
-    watch:{
-      markerCoordinates: function(newVal){
+    watch: {
+      markerCoordinates: function (newVal) {
         this.drawMarkers()
 
       }
     },
     methods: {
-      postjob: function(event) {
-        console.log("test event")
-      },
-      // postJob() {
-      //   debugger
-      //   this.$store.dispatch('postJob', marker.position)
-      // },
       closeAll() {
         this.markers.forEach(m => {
           m.infowindow.close();
         })
       },
-      drawMarkers(){
+      drawMarkers() {
         this.markerCoordinates.forEach((coord, index) => {
           let condition = this.$store.state.user.provider
-    if (!condition) {
-
-      var content =
-        '<div class="iw-container">' +
-        '<div>Job info here</div>' +
-        '<button id="postButton" class="ui-btn ui-mini" type="submit">POST JOB</button>' +
-        '</div>';
-    } else {
-      var content =
-      '<div class="iw-container">' +
-        '<div>Job info here</div>' +
-        '<button id="bidButton" class="ui-btn ui-mini" type="submit">BID on JOB</button>' +
-        '</div>';
-    }
-        
-        var locationInfowindow = new google.maps.InfoWindow({
-          content: content,
-          maxWidth: 300,
-        });
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(coord.latitude, coord.longitude),
-          title: "Stupid loop " + index,
-          visible: true,
-          infowindow: locationInfowindow
-        });
-        
-        marker.setMap(this.map);
-        
-        marker.addListener('click', (event) => {
-          let self = this
-          this.closeAll()
-          marker.infowindow.open(this.map, marker);
           if (!condition) {
-          var postButton = document.getElementById('postButton');
-          google.maps.event.addDomListener(postButton, 'click', function () {
-            console.log("New Job Posted")
-            self.$store.dispatch('postJob', marker.position)
-          });
-        }else{
-          var bidButton = document.getElementById('bidButton');
-          google.maps.event.addDomListener(bidButton, 'click', function () {
-            console.log("Job Bid Submitted")
-            self.$store.dispatch('bidJob', marker.position)
-          });
-        }
-        });
-        
-        google.maps.event.addListener(this.map, 'click', function () {
-          marker.infowindow.close();
-        });
 
-      });
+            var content =
+              '<div class="iw-container">' +
+              '<div>Job info here</div>' +
+              '<button id="postButton" class="ui-btn ui-mini" type="submit">POST JOB</button>' +
+              '</div>';
+          } else {
+            var content =
+              '<div class="iw-container">' +
+              '<div>Job info here</div>' +
+              '<button id="bidButton" class="ui-btn ui-mini" type="submit">BID on JOB</button>' +
+              '</div>';
+          }
+
+          var locationInfowindow = new google.maps.InfoWindow({
+            content: content,
+            maxWidth: 300,
+          });
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(coord.latitude, coord.longitude),
+            title: "Stupid loop " + index,
+            visible: true,
+            infowindow: locationInfowindow
+          });
+
+          marker.setMap(this.map);
+
+          marker.addListener('click', (event) => {
+            let self = this
+            this.closeAll()
+            marker.infowindow.open(this.map, marker);
+            if (!condition) {
+              var postButton = document.getElementById('postButton');
+              google.maps.event.addDomListener(postButton, 'click', function () {
+                console.log("New Job Posted")
+                self.$store.dispatch('postJob', marker.position)
+              });
+            } else {
+              var bidButton = document.getElementById('bidButton');
+              google.maps.event.addDomListener(bidButton, 'click', function () {
+                console.log("Job Bid Submitted")
+                self.$store.dispatch('bidJob', marker.position)
+              });
+            }
+          });
+
+          google.maps.event.addListener(this.map, 'click', function () {
+            marker.infowindow.close();
+          });
+
+        });
       }
     }
   };
