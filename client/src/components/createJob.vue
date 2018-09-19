b2e6874f6dad87ebc37ce15f4514294305d8e36c
k-cormack <k.cormack@gmail.com>
2018-09-19 13:53:07 -0600

    small css

--- client/src/components/createJob.vue
+++ client/src/components/createJob.vue
@@ -3,28 +3,22 @@
     <form class="pure-form pure-form-aligned" @submit.prevent="lookupJobLoc">
       <fieldset>
         <div class="pure-control-group">
-          <label for="street-address">Street Address</label>
           <input type="text" placeholder="Street Address" v-model="newJob.street" required>
-          <span class="pure-form-message-inline">This is a required field.</span>
         </div>
         <div class="pure-control-group">
-          <label for="city">City</label>
-        <input type="text" placeholder="City" v-model="newJob.city" required>
+          <input type="text" placeholder="City" v-model="newJob.city" required>
         </div>
         <div class="pure-control-group">
-          <label for="State">State</label>
-        <input type="text" placeholder="State" v-model="newJob.state" required>
+          <input type="text" placeholder="State" v-model="newJob.state" required>
         </div>
         <div class="pure-control-group">
-          <label for="zip-code">Zip Code</label>
-        <input type="text" placeholder="Zip Code" v-model="newJob.zip">
-        </div>
-        <div class="pure-controls">
-            <label for="cb" class="pure-checkbox">
-                <input id="cb" type="checkbox"> I've read the terms and conditions
-            </label>
-        <button type="submit" class="pure-button pure-button-primary">Verify Plow Location</button>
-        </div>
+          <input type="text" placeholder="Zip Code" v-model="newJob.zip">
+        </div>        
+          <label for="cb" class="pure-checkbox">
+            <input id="cb" type="checkbox"> I've read the terms and conditions
+          </label>
+          <button type="submit" class="pure-button pure-button-primary">Verify Plow Location</button>
+        
       </fieldset>
     </form>
   </div>
@@ -70,9 +64,11 @@
   };
 </script>
 <style scoped>
-  label, span {
+  label,
+  span {
     color: white
   }
- 
-
+  .pure-controls {
+    
+  }
 </style>
