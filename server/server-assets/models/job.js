6922705dc43a8279373c48f4f9907255bf8928c5
k-cormack <k.cormack@gmail.com>
2018-09-28 13:56:47 -0600

    lat/long for jobs required

--- server/server-assets/models/job.js
+++ server/server-assets/models/job.js
@@ -7,19 +7,19 @@ let ObjectId = Schema.Types.ObjectId
   
 let schema = new Schema({
   customerId: { type: ObjectId,ref:"customer", required: true },
-  customerName: { type: String, required: true },
-  street: { type: String, required: true },
-  state: { type: String, required: true },
-  city: { type: String, required: true },
-  lat: {type: String},
-  long: {type: String},
-  customerEmail: { type: String, required: true },
-  customerPhone: { type: String, required: true },
   created: { type: Number, required: true, default: Date.now() },
   pending: { type: Boolean, default: true, required: true },
   active: { type: Boolean, default: false, required: true},
   completed: {type: Boolean, default: false},
   messages: [],
+  customerEmail: { type: String, required: true },
+  customerName: { type: String, required: true },
+  street: { type: String, required: true },
+  state: { type: String, required: true },
+  city: { type: String, required: true },
+  lat: {type: String, required: true},
+  long: {type: String, required: true},
+  customerPhone: { type: String, required: true },
 })
 
 
